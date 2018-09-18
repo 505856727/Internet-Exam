86a52f31c15dbc44233b5925ef8f3df99feb6568
505856727 <30371192+505856727@users.noreply.github.com>
2018-09-19 00:02:57 +0800

    jiaer

---
+++ unitylistener/unitylistener/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace NetworkServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Socket listenfd = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipAdr = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipEp = new IPEndPoint(ipAdr, 1234);
+            listenfd.Bind(ipEp);
+            listenfd.Listen(0);
+            Console.WriteLine("服务器启动");
+            while (true)
+            {
+                Socket connfd = listenfd.Accept();
+                Console.WriteLine("服务器已经连接");
+                byte[] readBuff = new byte[1024];
+                int count = connfd.Receive(readBuff);
+                string str = Encoding.UTF8.GetString(readBuff, 0, count);
+                Console.WriteLine("服务器接收" + str);
+                byte[] bytes = Encoding.Default.GetBytes("serv echo" + str);
+                connfd.Send(bytes);
+            }
+        }
+    }
+}
